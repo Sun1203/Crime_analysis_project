a469006860e6f115f517cabbc9e0ed34ddf456dd
khk <jasun1208@naver.com>
2021-07-16 16:05:39 +0900

    범죄 검거까지 기간 데이터 정제

--- pre_processing.py
+++ pre_processing.py
@@ -5,6 +5,7 @@ class data:
 
     def process():
 
+        # 범죄 발생과 범죄 검거 상황 정제
 
         df = pd.read_csv("./dataset/범죄검거상황.csv", encoding='cp949', header=2, thousands=',')
         
@@ -23,4 +24,26 @@ class data:
         df["여자"] = df["여자"].astype(int)
         df["미상"] = df["미상"].astype(int)
         df["검거율(%)"] = df["검거율(%)"].round(3)
-        
+
+        # 범죄 검거까지 기간 데이터 정제
+
+        df1 = pd.read_csv("./dataset/범죄검거기간.csv", encoding='cp949', header=2, thousands=',')
+
+        df1 = df1[df1["분류.1"] != "강간·강제추행"]
+
+        del df1["분류"]
+        del df1["원자료.10"]
+        del df1["원자료"]
+        del df1["No"]
+
+        df1.columns = ['통계년도', '범죄종류', '1일이내', '2일이내', '3일이내', '10일이내', '1개월이내', '3개월이내', '6개월이내', '1년이내', '1년초과']
+
+        df1["1일이내"] = df1["1일이내"].astype(int)
+        df1["2일이내"] = df1["2일이내"].astype(int)
+        df1["3일이내"] = df1["3일이내"].astype(int)
+        df1["10일이내"] = df1["10일이내"].astype(int)
+        df1["1개월이내"] = df1["1개월이내"].astype(int)
+        df1["3개월이내"] = df1["3개월이내"].astype(int)
+        df1["6개월이내"] = df1["6개월이내"].astype(int)
+        df1["1년이내"] = df1["1년이내"].astype(int)
+        df1["1년초과"] = df1["1년초과"].astype(int)
