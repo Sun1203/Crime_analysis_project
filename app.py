9bca8a96c8fc805953dc746c259f8dc4909f514d
khk <jasun1208@naver.com>
2021-07-21 09:37:29 +0900

    merge및 회원가입 페이지 및 로그인 페이지 추가

--- app.py
+++ app.py
@@ -1,33 +1,82 @@
 from flask import Flask, request, render_template, jsonify
 from flask_jwt_extended import *
-from memo import memo
+from DB_dao import *
+from dto import *
+from Elk_python import *
+# from memo import memo
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG=True,
+    JWT_SECRET_KEY="I'M IML"
+)
 
-@app.route('/', methods=["GET"])
+jwt=JWTManager(app)
+
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method=="GET":
+        return render_template('index.html')
+    else:
+        data = book(request.form.get("id"))
+        data = jsonify(data)
+        print(type(data))
+        return data
+
 
 @app.route('/info', methods=['GET', 'POST'])
 def about():
     if request.method == "GET":
         return render_template('info.html')
 
-        # <iframe src="http://localhost:5601/goto/9bc00ce7159558ceb9864d06d845b86f" height="600" width="800"></iframe>
     elif request.method == "POST":
         return '{"img01" : "http://localhost:5601/goto/9bc00ce7159558ceb9864d06d845b86f"}'
 
 @app.route('/info1', methods=['GET', 'POST'])
 def about1():
 
-        # <iframe src="http://localhost:5601/goto/e660241fa417e58a78414d910c158ac0" height="600" width="800"></iframe>
     if request.method == "POST":
         return '{"img02" : "http://localhost:5601/goto/e660241fa417e58a78414d910c158ac0"}'
 
+
+
 @app.route('/join', methods=['GET','POST'])
-def join():
-    return render_template('join.html')
+def join1():
+    if request.method =="GET":
+
+        return render_template('join.html')
+    else:
+        dao = InfoDAO()
+        dto = userjoindto(request.form.get("userid"), request.form.get("userpw"), request.form.get("username"))
+        dao.insertjoin(dto)
+        return render_template('join.html')
+
+
+@app.route('/login', methods=['GET'])
+def move():
+    return render_template('login.html')
+
+
+
+@app.route('/login', methods=['POST'])
+def login1():
+    id = request.form.get("userid")
+    pw = request.form.get("userpw")
+    dao= InfoDAO()
+
+    if id == dao.login(request.form.get("userid")) & pw == dao.login(request.form.get('userpw')):
+        return jsonify(
+            result="200",
+            access_token=create_access_token(identity=id)
+        )
+    
+
+
+
+
 
 @app.route('/map', methods=['GET'])
 def map():
